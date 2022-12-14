45347421df8edb5c45c1b7f1c3bf94e683786fbd
Tommy <tommy.brumley95@gmail.com>
2022-12-13 20:39:45 -0500

    brainfart on bs -wip

---
+++ src/katas/bs.clj
@@ -0,0 +1,27 @@
+(ns katas.bs)
+
+;; Write me a function that takes a sequential data structure of integers and returns them in order
+;; do not use built in sort function!
+
+(defn foo
+  "a function that takes a sequential data structure of integers and returns them in order"
+  [x]
+    ;; (let [:result vec]
+    ;; (when (< a b))
+    ;; place it in the front!
+  )
+
+;; (let [arg1 arg2]
+;;  (if (> arg1 arg2)
+;;         else))
+
+
+
+
+(comment
+  ;
+  (def seq-data1 [3 5 4 2 1])
+  (def result1 [1 2 3 4 5])
+  (map (bar x))
+  ;
+  )
