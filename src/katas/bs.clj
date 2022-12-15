b8c6267dac2312fb0d9ea5883e4cc0610c5f1327
Tommy <tommy.brumley95@gmail.com>
2022-12-14 22:34:28 -0500

    implement helper function

--- src/katas/bs.clj
+++ src/katas/bs.clj
@@ -5,23 +5,31 @@
 
 (defn foo
   "a function that takes a sequential data structure of integers and returns them in order"
-  [x]
-    ;; (let [:result vec]
-    ;; (when (< a b))
-    ;; place it in the front!
+  [coll]
   )
-
-;; (let [arg1 arg2]
-;;  (if (> arg1 arg2)
-;;         else))
-
-
-
+ 
+(defn helper [seq-data]
+  (let [[foo bar] seq-data]
+    (if (< foo bar)
+      (vec '(foo bar))
+      [bar foo])))
 
 (comment
   ;
-  (def seq-data1 [3 5 4 2 1])
+  (helper seq-data2)
+  (let [[x y z] my-vector]
+    (println x y z))
+   ;= 1 2 3
+  
+  (def seq-data1 [5 3 4 2 1])
   (def result1 [1 2 3 4 5])
-  (map (bar x))
+  
+  (def seq-data2 [2 1])
+  (def result2 [1 2])
+  
+  (def seq-data3 [3 1 2]) 
+  
+  (first (rest seq-data2))
+  
   ;
   )
