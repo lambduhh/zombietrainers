362306bf8c5bac19bf5789c1f70bc74f94723e14
Wendy Randolph <wendy.randolph5@gmail.com>
2023-02-04 23:30:23 -0500

    removeold data, add design notes

--- src/fatcat/mockdata.clj
+++ src/fatcat/mockdata.clj
@@ -1,48 +1,7 @@
 (ns fatcat.mockdata)
 
-(def fatcat
-  "An overweight house cat, needs to lose some pounds. "
-  {:weight-in-lbs 16                                        ;;int
-   :sex           "male"                                    ;;string
-   :age           10                                        ;;int
-   :name          "chester"                                 ;;string
-   :color         "orange"                                  ;;string
-   :activity      :indoor                                   ;;keyword
-   :goal          :maintain})                               ;;keyword
-
-(def skinny-cat
-  "this cat is fair to middlin weight"
-  {:weight-in-lbs 7
-   :sex           "female"
-   :age           5
-   :name          "BB"
-   :color         "black and white"
-   :breed         "calico"
-   :activity      :outdoor
-   :goal          :gain
-   :brand         "Purina"
-   :last-purchase "10 06 22"
-   :wet?          true})
-
-(def regular-cat
-  "A regular-weight house cat, needs to maintain weight."
-  {:weight-in-lbs 9                                         ;;int
-   :sex           "female"                                  ;;string
-   :age           15                                        ;;int
-   :name          "toffee"                                  ;;string
-   :color         "tortoise-shell"                          ;;string
-   :activity      :indoor                                   ;;keyword
-   :goal          :maintain})                               ;;keyword
-
-(def bags-of-fewd "In lbs, integer that represents the common sizes of food available for purchase in the US. "
-  {:small   3.5
-   :medium  7
-   :large   16
-   :x-large 32})
-
-; Wendy's Pet Food Calculator
+; Pet Food Calculator
 ; improvements: 
-; - make generic for any pet/animal
 ; - provide several units of weight: kg, g, lb, oz so more flexible
 ; - gives result of how many bags of food need to purchase in a given time period (ex. how many bags per month)
 
@@ -61,3 +20,29 @@
   (/ bag-grams (* weight-kgs grams-per-kg-of-pet)))
 
 (days-per-bag toffee-cat liveclear-turkey-small) ;; => 28.3219512195122
+
+
+(def new-cat {:weight 
+              {:unit 
+               {:kg 4.1 :lb 9}
+               }})
+;; => #'fatcat.mockdata/new-cat
+
+; data to design:
+
+; food remaining / last delivery date
+
+; rewrite bag data to mirrow new-cat
+
+
+; fn to design:
+
+; fn that returns how much food to feed cat each day (servings per day)
+
+; is low?
+
+; fn that returns daily servings per bag (can modify existing)
+
+; conversion from lb to g ?
+
+; conversion from g to lb ?
