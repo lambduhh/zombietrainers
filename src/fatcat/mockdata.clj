cab1a7a91f0a1a31fef3cf63550a92710afc9f09
Wendy Randolph <wendy.randolph5@gmail.com>
2023-02-09 20:05:31 -0500

    wip data design and fn design

--- src/fatcat/mockdata.clj
+++ src/fatcat/mockdata.clj
@@ -21,28 +21,47 @@
 
 (days-per-bag toffee-cat liveclear-turkey-small) ;; => 28.3219512195122
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def new-cat {:weight 
-              {:unit 
-               {:kg 4.1 :lb 9}
-               }})
-;; => #'fatcat.mockdata/new-cat
+(def new-cat 
+  {:weight {:unit {:kg 4.1 :lb 9}}})
 
-; data to design:
+(def liveclear-turkey
+ {:weight {:unit {:kg 1.4515 :lb 3.2 :g 1451.5}}
+ ; :food-remaining {:unit {:kg (* x :lb) :g (* x :lb)}} ; does this idea belong here?
+ ; :last-delivered x                                ; does this idea belong here?
+ ; :is-low?                                        ; does this belong here?
+  :grams-per-kg-of-cat 12.5})
 
-; food remaining / last delivery date
+(defn daily-serving
+"Takes weight of cat in kg and multiplies it by how many grams of food per kg of cat. Returns how much food to serve cat each day."
+  [{:keys [weight]}
+   {:keys [grams-per-kg-of-cat]}]
+  (* kg grams-per-kg-of-cat))
 
-; rewrite bag data to mirrow new-cat
+{{:keys [kg]} {{:keys [unit]}} :weight}
+{{:keys [kg]} :unit}
 
+;; ex: {{:keys [width height]} :resolution}
+;; see "associative destructuring"
 
-; fn to design:
+(daily-serving new-cat liveclear-turkey)
 
-; fn that returns how much food to feed cat each day (servings per day)
+;;;;;;;; data to design:
+
+; MAYBE food remaining and/or last delivery date
+
+; maybe make each value for the weight key be a fn for conversion
+
+;;;;;;; fn to design:
+
+; working on it: fn that returns how much food to feed cat each day (servings per day)
 
 ; is low?
 
 ; fn that returns daily servings per bag (can modify existing)
 
-; conversion from lb to g ?
+; lb to g ?
 
-; conversion from g to lb ?
+; g to lb ?
+
